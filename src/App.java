37dbd1e0eb14ca635bb5e6e3029b1ab971b615ab
John Tigre <cuji309@gmail.com>
2024-09-27 12:51:06 -0500

    Se finalizo el proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+public class App {public static void main(String[] args) throws Exception {
+
+    boolean ascendente = true;
+    boolean descendente = false;
+
+    System.out.println("Metodo de ordenamiento Seleccion");
+    MetodosOrdenamiento mO = new MetodosOrdenamiento();
+    int[] arreglo = {5, 2, 8, 1, 9, 3, 6, 4, 7};
+    mO.printArreglo(arreglo);
+     
+    //System.out.println("Arreglo ordenado");
+    //arreglo = mO.sortBySeleccion(arreglo);
+   // mO.printArreglo(arreglo);
+
+    mO.sortBySeleccionM(arreglo, ascendente);
+    mO.printArreglo(arreglo);
+
+    mO.sortBySeleccionM(arreglo, descendente);
+    mO.printArreglo(arreglo);
+
+}}
